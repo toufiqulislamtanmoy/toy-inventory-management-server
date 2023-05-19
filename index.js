8fa7a1726401d03e22df3244c2d3b95ac66bf215
toufiqulislamtanmoy <tittanmoy@gmail.com>
2023-05-19 15:29:52 +0600

    delete funtion done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const port = process.env.PORT || 5000
@@ -77,6 +77,16 @@ async function run() {
             res.send(result);
             console.log(toy)
         })
+
+        // DELETE a particular product 
+
+        app.delete('/toys/:id', async (req, res) => {
+            const id = req.params.id;
+            console.log(id)
+            const query = { _id: new ObjectId(id) }
+            const result = await allToyCollections.deleteOne(query)
+            res.send(result);
+          })
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
